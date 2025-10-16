d4f32b97add57550dd6ee35f5610c8dc0f3006ec
Miriam Romero <jguerod357@g.educaand.es>
2025-10-16 12:39:15 +0200

    Añadida la base de la actividades

--- src/main/java/com/example/add/AddApplication.java
+++ src/main/java/com/example/add/AddApplication.java
@@ -1,14 +1,21 @@
 package com.example.add;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class AddApplication {
+public class AddApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddApplication.class, args);
 
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Hola Mundo");
+	}
 }
